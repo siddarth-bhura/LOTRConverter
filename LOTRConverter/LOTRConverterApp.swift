8d94b22752b490ee08eb2cda1129ca18d10d2021
Siddarth Bhura <sidjain.sb@gmail.com>
2025-10-07 15:15:37 +0530

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Siddarth Bhura on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
